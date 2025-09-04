fbe03e64d8892e242d76353154f1b4ced2df4b50
Briama Martinez <Briana.gmartinez@outlook.com>
2025-09-04 09:04:18 -0500

    Initial portfolio commit

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,14 +4,14 @@ const projects = [
     title: "Bookstore App",
     tags:["React","API"],
     url: "https://github.com/bgmartine/bookstore-app",
-    image: "/src/assets/bookstore.jpg"
+    image: "/my-portfolio/src/assets/bookstore.jpg"
   },
   {
     id: 2,
     title: "Weather App",
     tags:["Vite","Fetch"],
     url: "https://github.com/bgmartine/Weather-app",
-    image: "/src/assets/weather.jpg"
+    image: "/my-portfolio/src/assets/weather.jpg"
   }
 ];
 
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,14 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "node:url";
 import { dirname, resolve } from "node:path";
-
-// ESM-safe __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: "/my-portfolio/", 
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-    },
+    alias: { "@": resolve(__dirname, "src") },
   },
 });
